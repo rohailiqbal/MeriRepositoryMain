c9506efbd4fc25906509992137fd7df512cb830f
rohailiqbal <30309646+rohailiqbal@users.noreply.github.com>
2017-07-20 16:46:30 +0500

    Sample Project Uplodaed
    Sample Project Uplodaed

---
+++ GridSelectedItemsMVVM/MyDataContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfApplication1
+{
+    public class MyDataContext : INotifyPropertyChanged
+    {
+        IEnumerable<MyObject> _Data;
+        public IEnumerable<MyObject> Data
+        { 
+            get
+            {
+                if (_Data == null)
+                {
+                    _Data = new ObservableCollection<MyObject>(from i in Enumerable.Range(0, 50) select new MyObject() { ID = i , Name = String.Format("Name{0}", i) });
+                }
+                return _Data;
+            }
+        }
+
+        ObservableCollection<MyObject> _SelectedItems;
+        public ObservableCollection<MyObject> SelectedItems
+        {
+            get
+            {
+                if (_SelectedItems == null)
+                {
+                    _SelectedItems = new ObservableCollection<MyObject>();
+                }
+                return _SelectedItems;
+            }
+        }
+
+        MyObject _SelectedItem;
+        public MyObject SelectedItem
+        {
+            get
+            {
+                return _SelectedItem;
+            }
+            set
+            {
+                if (_SelectedItem != value)
+                {
+                    _SelectedItem = value;
+
+                    OnPropertyChanged("SelectedItem");
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
